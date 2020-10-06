30c38659ca17088946d006478281f33d62f79a36
Dennis Witzel <67911876+denniswitzel@users.noreply.github.com>
2020-10-06 11:32:48 +0200

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,7 +4,7 @@ export default function useFetch(urlName) {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    fetch(`http://192.168.178.53:5000/${urlName}`)
+    fetch(`https://my-json-server.typicode.com/denniswitzel/capstone-project/${urlName}`)
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch((error) => {
